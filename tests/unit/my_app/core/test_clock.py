c9b66d12307485aeefbc9519e48c11090748603f
stormy_mm <maksutenkomihail@gmail.com>
2026-02-23 00:24:18 +0300

    добавлены тесты на проверку парсинга даты

---
+++ tests/unit/my_app/core/test_clock.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+import pytest
+
+from my_app.cli.date_parser import ParsingDate
+from my_app.common import exceptions as e
+
+
+class TestParsingDate:
+    """Класс для проверки парсинга даты"""
+
+    def test_parsing_date(self):
+        """Тест для проверки парсинга корректной даты"""
+        assert (ParsingDate("20 3 2026 12 00").date ==
+                         datetime(2026, 3, 20, 12, 0, 0, tzinfo=ZoneInfo('UTC')))
+        assert (ParsingDate("20 3 2026").date ==
+                         datetime(2026, 3, 20, 0, 0, 0, tzinfo=ZoneInfo('UTC')))
+
+    def test_parsing_defective_date(self):
+        """Тест для проверки парсинга некорректной даты"""
+        with pytest.raises(e.ParsingError):
+            ParsingDate("12 01 2026 12:00:00")
+        with pytest.raises(e.ParsingError):
+            ParsingDate("03/14/2026")
