643c823b4f6d01a831e9b605c1dc7721233cc688
stormy_mm <maksutenkomihail@gmail.com>
2026-02-22 12:28:13 +0300

    парсинг

--- DOMAINS/parsing.py
+++ DOMAINS/parsing.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+
 from DOMAINS.time_clock import Clock
 
 
 class ParsingDate:
     """Класс для парсинга даты"""
 
-    def __init__(self, date_string):
+    def __init__(self, date_string: str = "", view_date: datetime = datetime):
         """Функция для инициализации класса"""
-        self._date = Clock.set_deadline(date_string)
+        self._date = Clock.set_deadline(date_string) if date_string != "" else ...
+        self._view_date = view_date
 
     @property
     def date(self):
@@ -16,4 +19,4 @@ class ParsingDate:
     @property
     def date_format(self):
         """Функция для получения даты в формате строки"""
-        return self._date.strftime("%d.%m.%Y %H:%M:%S")
+        return self._view_date.strftime("%d.%m.%Y %H:%M:%S")
